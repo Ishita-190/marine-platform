72674926940e9c42194403a97bb00db95ee3eea1
Ishita Sati <ishasati19@gmail.com>
2025-09-09 22:25:54 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => (
+  <nav className="bg-blue-600 p-4 text-white text-xl font-bold">
+    Marine Data Platform
+  </nav>
+);
+
+export default Navbar;
