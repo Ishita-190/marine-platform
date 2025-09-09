ee36662550c3a16c986b0cbf7897b880cac5b055
Ishita Sati <ishasati19@gmail.com>
2025-09-09 22:47:25 +0530

    Update MapView.js

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -2,17 +2,20 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const MapView = () => {
+function MapView() {
+  const keralaCoords = [9.9312, 76.2673]; // Example location
+
   return (
-    <MapContainer center={[10, 75]} zoom={5} style={{ height: "400px", width: "100%" }}>
+    <MapContainer center={keralaCoords} zoom={6} style={{ height: "400px", width: "100%" }}>
       <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[9.9312, 76.2673]}>
-        <Popup>Kerala Coast - Indian Mackerel</Popup>
+      <Marker position={keralaCoords}>
+        <Popup>Indian Mackerel - Kerala Coast</Popup>
       </Marker>
     </MapContainer>
   );
-};
+}
 
 export default MapView;
